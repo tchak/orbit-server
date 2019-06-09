01e10de0cb0c0e1423f016370483371b349819ed
Paul Chavard <git@paul.chavard.net>
2019-06-09 17:26:21 +0200

    Test basic GraphQL queries

--- test/support/fastify-plugin-shared.ts
+++ test/support/fastify-plugin-shared.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { test } from 'qunit';
+import { module, test } from 'qunit';
 import { FastifyInstance, HTTPInjectOptions } from 'fastify';
 
 export interface Subject {
@@ -7,95 +7,128 @@ export interface Subject {
 }
 
 export default function(subject: Subject) {
-  test('get planets (empty)', async function(assert: Assert) {
-    const response = await getPlanets(subject.fastify);
+  module('jsonapi', function() {
+    test('get planets (empty)', async function(assert: Assert) {
+      const response = await getPlanets(subject.fastify);
 
-    assert.equal(response.status, 200);
-    assert.deepEqual(response.body, { data: [] });
-  });
-
-  test('create planet', async function(assert: Assert) {
-    const response = await createEarth(subject.fastify);
-
-    assert.equal(response.status, 200);
-    assert.equal(response.body.data.type, 'planets');
-    assert.ok(response.body.data.id);
-    assert.deepEqual(response.body.data.attributes, { name: 'Earth' });
-  });
-
-  test('get planets', async function(assert: Assert) {
-    await createEarth(subject.fastify);
-    const response = await getPlanets(subject.fastify);
-
-    assert.equal(response.status, 200);
-    assert.equal(response.body.data.length, 1);
-  });
-
-  test('get planet', async function(assert: Assert) {
-    const { body } = await createEarth(subject.fastify);
-    const id = body.data.id;
-
-    const response = await getPlanet(subject.fastify, id);
-
-    assert.equal(response.status, 200);
-    assert.deepEqual(response.body.data, {
-      type: 'planets',
-      id,
-      attributes: {
-        name: 'Earth'
-      }
+      assert.equal(response.status, 200);
+      assert.deepEqual(response.body, { data: [] });
     });
-  });
 
-  test('update planet', async function(assert: Assert) {
-    const { body } = await createEarth(subject.fastify);
-    const id = body.data.id;
+    test('create planet', async function(assert: Assert) {
+      const response = await createEarth(subject.fastify);
 
-    const response = await request(subject.fastify, {
-      method: 'PATCH',
-      url: `/planets/${id}`,
-      payload: {
-        data: {
-          id,
-          type: 'planets',
-          attributes: {
-            name: 'Earth 2'
+      assert.equal(response.status, 200);
+      assert.equal(response.body.data.type, 'planets');
+      assert.ok(response.body.data.id);
+      assert.deepEqual(response.body.data.attributes, { name: 'Earth' });
+    });
+
+    test('get planets', async function(assert: Assert) {
+      await createEarth(subject.fastify);
+      const response = await getPlanets(subject.fastify);
+
+      assert.equal(response.status, 200);
+      assert.equal(response.body.data.length, 1);
+    });
+
+    test('get planet', async function(assert: Assert) {
+      const { body } = await createEarth(subject.fastify);
+      const id = body.data.id;
+
+      const response = await getPlanet(subject.fastify, id);
+
+      assert.equal(response.status, 200);
+      assert.deepEqual(response.body.data, {
+        type: 'planets',
+        id,
+        attributes: {
+          name: 'Earth'
+        }
+      });
+    });
+
+    test('update planet', async function(assert: Assert) {
+      const { body } = await createEarth(subject.fastify);
+      const id = body.data.id;
+
+      const response = await request(subject.fastify, {
+        method: 'PATCH',
+        url: `/planets/${id}`,
+        payload: {
+          data: {
+            id,
+            type: 'planets',
+            attributes: {
+              name: 'Earth 2'
+            }
           }
         }
-      }
+      });
+
+      assert.equal(response.status, 200);
+
+      const {
+        status,
+        body: { data }
+      } = await getPlanet(subject.fastify, id);
+
+      assert.equal(status, 200);
+      assert.deepEqual(data, {
+        type: 'planets',
+        id,
+        attributes: {
+          name: 'Earth 2'
+        }
+      });
     });
 
-    assert.equal(response.status, 200);
+    test('remove planet', async function(assert: Assert) {
+      const { body } = await createEarth(subject.fastify);
+      const id = body.data.id;
 
-    const {
-      status,
-      body: { data }
-    } = await getPlanet(subject.fastify, id);
+      const response = await request(subject.fastify, {
+        method: 'DELETE',
+        url: `/planets/${id}`
+      });
 
-    assert.equal(status, 200);
-    assert.deepEqual(data, {
-      type: 'planets',
-      id,
-      attributes: {
-        name: 'Earth 2'
-      }
+      assert.equal(response.status, 200);
+
+      const { status } = await getPlanet(subject.fastify, id);
+
+      assert.equal(status, 404);
     });
   });
 
-  test('remove planet', async function(assert: Assert) {
-    const { body } = await createEarth(subject.fastify);
-    const id = body.data.id;
+  module('graphql', function() {
+    test('get planets (empty)', async function(assert: Assert) {
+      const response = await getGQLPlanets(subject.fastify);
 
-    const response = await request(subject.fastify, {
-      method: 'DELETE',
-      url: `/planets/${id}`
+      assert.equal(response.status, 200);
+      assert.deepEqual(response.body.data, { planets: [] });
     });
 
-    assert.equal(response.status, 200);
+    test('get planets', async function(assert: Assert) {
+      await createEarth(subject.fastify);
+      const response = await getGQLPlanets(subject.fastify);
 
-    const { status } = await getPlanet(subject.fastify, id);
+      assert.equal(response.status, 200);
+      assert.equal(response.body.data.planets.length, 1);
+    });
 
-    assert.equal(status, 404);
+    test('get planet', async function(assert: Assert) {
+      const { body } = await createEarth(subject.fastify);
+      const id = body.data.id;
+
+      const response = await getGQLPlanet(subject.fastify, id);
+
+      assert.equal(response.status, 200);
+      assert.deepEqual(response.body.data.planet, {
+        __typename: 'Planet',
+        id,
+        name: 'Earth'
+      });
+    });
   });
 }
 
@@ -133,5 +166,25 @@ function getPlanet(fastify: FastifyInstance, id: string) {
 function getPlanets(fastify: FastifyInstance) {
   return request(fastify, {
     url: '/planets'
+  });
+}
+
+function getGQLPlanets(fastify: FastifyInstance) {
+  return request(fastify, {
+    method: 'POST',
+    url: '/graphql',
+    payload: {
+      query: '{ planets { __typename id name } }'
+    }
+  });
+}
+
+function getGQLPlanet(fastify: FastifyInstance, id: string) {
+  return request(fastify, {
+    method: 'POST',
+    url: '/graphql',
+    payload: {
+      query: `{ planet(id: "${id}") { __typename id name } }`
+    }
   });
 }
